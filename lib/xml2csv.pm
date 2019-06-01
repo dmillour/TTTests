9e249eb01d2e61b8a8e87ed7b9fad96f0f929e80
dmillour <daniel_millour@hotmail.com>
2019-06-01 17:00:05 +0200

    commit

--- lib/xml2csv.pm
+++ lib/xml2csv.pm
@@ -3,19 +3,58 @@ use Exporter;
 use strict;
 use warnings;
 use XML::Twig;
+use feature qw(say);
 
-our @EXPORT= qw(parse_xml);
+our @EXPORT= qw(parse_xml convert2csv);
 
 sub parse_xml {
   my ($filename) = @_;
-  my $twig_ref=XML::Twig->new();
+  my $data_ref = {};
+  my $twig_ref=XML::Twig->new(
+    twig_handlers =>
+    { '/configuration' => sub {
+        $data_ref->{destinations}=[split /,/ , uc $_->att('destinations')];},
+     '/configuration/database' => sub {
+       my $database_ref= { name => $_->att('name'), records =>[ map {_get_children($_)} $_->children()]};
+       push @{$data_ref->{databases}}, $database_ref;}
+    }
+  );
   $twig_ref->parsefile($filename);
-  return $twig_ref;
+  return $data_ref;
 }
 
+sub _get_children {
+  my ($elem_ref) = @_;
+  my $record_ref = {_fields => $elem_ref->atts(), _name => uc $elem_ref->gi(),_children => []} ;
+  foreach my $child_ref ($elem_ref->children()) {
+    push @{$record_ref->{_children}}, _get_children($child_ref);
+  }
+  return $record_ref;
+}
 
+sub convert2csv {
+  my ($data_ref, $fh) = @_;
+  unless ($fh) {
+    $fh = *STDOUT;
+  };
+  foreach my $record_ref (@{$data_ref->{records}}) {
+    _print_records($record_ref,$fh);
+  }
 
+}
 
-
+sub _print_records {
+  my ($data_ref, $fh) = @_;
+  print $fh $data_ref->{_name};
+  foreach my $key ( keys %{$data_ref->{_fields}}) {
+    my $value = $data_ref->{_fields}{$key};
+    $key = uc $key;
+    print $fh ",$key='$value'";
+  }
+  print $fh "\n";
+  foreach my $record_ref (@{$data_ref->{_children}}) {
+    _print_records($record_ref,$fh);
+  }
+}
 #EOF
 1;
--- t/xml2csv.t
+++ t/xml2csv.t
@@ -1,10 +1,15 @@
 use strict;
 use warnings;
-
+use lib 'C:\Users\dmillour\TTTests\lib';
 use Test::More tests => 1;
-use xml2csv qw(parse_xml);
+use xml2csv qw(parse_xml convert2csv);
+use Data::TreeDumper;
 
 my $filename_ok='t\xml\xml_ok.xml';
 
-my $twig_ref= xml2csv::parse_xml($filename_ok);
-ok($twig_ref);
+my $data_ref= xml2csv::parse_xml($filename_ok);
+
+ok($data_ref);
+
+print DumpTree($data_ref,'data');
+xml2csv::convert2csv($data_ref->{databases}[0]);
