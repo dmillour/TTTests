4cf51c31eed8cb50d6bec760ebef97eaefe23880
Daniel Millour <daniel.millour@ge.com>
2019-09-03 17:55:01 +0200

    add error handling for bad images add special cat

--- unarchive.pl
+++ unarchive.pl
@@ -11,6 +11,7 @@ use Digest::file qw(digest_file_hex);
 use File::Path qw(make_path remove_tree);
 use Data::TreeDumper;
 use MIME::Types;
+use File::Copy 'copy';
 
 use Image::Resize;
 use MIME::Base64;
@@ -19,13 +20,25 @@ use Thread::Queue;
 use BerkeleyDB;
 use Getopt::Long;
 
+use XML::Twig;
 
-my $source_folderpath = './input';
-my $tmp_folderpath    = './tmp';
-my $dst_folderpath    = './output';
-my $buffer_size_down  = 16;
-my $buffer_size_up    = 3;
-my $threads_nb        = 6;
+my $twig_ref = XML::Twig->new();
+$twig_ref->parsefile('config.xml');
+my $twig_root_ref = $twig_ref->root();
+
+
+my $source_folderpath = $twig_root_ref->first_child('source')->text();
+my $tmp_folderpath    = $twig_root_ref->first_child('tmp')->text();
+my $dst_folderpath    = $twig_root_ref->first_child('dst')->text();
+my $buffer_size_down  = $twig_root_ref->first_child('buffer_size_down')->text();
+my $buffer_size_up    = $twig_root_ref->first_child('buffer_size_up')->text();
+my $threads_nb        = $twig_root_ref->first_child('threads_nb')->text();
+
+my %special_folders;
+for my $folder_elem_ref ($twig_root_ref->descendants('folder')) {
+   $special_folders{$folder_elem_ref->att('key')} = [$folder_elem_ref->att('title'), $folder_elem_ref->text()];
+}
+
 
 my %catalog_paths = (accepted => 'accepted.dbm', rejected => 'rejected.dbm', archives => 'archives.dbm');
 my %catalog;
@@ -127,11 +140,17 @@ sub producepic {
       my $y     = $item_ref->[3];
 
       last unless -f $file;
-      my $im = Image::Resize->new($file);
-      my $gd = $im->resize($x, $y);
+      my $res;
+      my $ok = 0;
+      eval {
+         my $im = Image::Resize->new($file);
+         my $gd = $im->resize($x, $y);
+         $res = encode_base64($gd->png);
+         $ok  = 1;
+      };
 
       #Tk needs base64 encoded image files
-      $queue_out->enqueue([$index, encode_base64($gd->png)]);
+      $queue_out->enqueue([$index, $ok, $res]);
    }
 }
 
@@ -146,7 +165,7 @@ sub scan_sourcefolder {
          my $extention = lc $1;
          next unless grep { $extention eq $_ } @legal_archive_names;
          my $dest_tmp_folderpath = unarchive($filename, $extention);
-         my @files               = sort { $a->[1] cmp $b->[1] } add($filename, $tmp_folderpath);
+         my @files               = sort { $a->{filename} cmp $b->{filename} } add($filename, $tmp_folderpath);
          my $file_nb             = @files;
          set_archive_remains($filename, $file_nb);
          if ($file_nb) {
@@ -195,7 +214,7 @@ sub add {
       return @results unless $mt->mimeTypeOf($file_path) =~ /^image/;
       my $hash = digest_file_hex($file_path, 'SHA1');
       unless (is_file_known($hash)) {
-         push @results, [$hash, $filename, 0];
+         push @results, {hash => $hash, filename => $filename, status => 0};
       }
    }
    return @results;
@@ -204,14 +223,18 @@ sub add {
 sub move {
    my ($filename, @files) = @_;
    for my $item_ref (@files) {
-      my $src_filepath = File::Spec->catfile($tmp_folderpath, $item_ref->[1]);
-      my $dst_filepath = File::Spec->catfile($dst_folderpath, rename_dest_files(File::Spec->abs2rel($item_ref->[1], $filename)));
+      my $src_filepath = File::Spec->catfile($tmp_folderpath, $item_ref->{filename});
+      my $dst_filepath = File::Spec->catfile($dst_folderpath, rename_dest_files(File::Spec->abs2rel($item_ref->{filename}, $filename)));
+      if ($item_ref->{special}) {
+         my $special_folderpath = File::Spec->catdir($dst_folderpath, $special_folders{$item_ref->{special}}[1]);
+         copy_special_file($src_filepath, $special_folderpath);
+      }
       say "copying source:'$src_filepath' \tto '$dst_filepath'";
       $dst_filepath = check_dest($dst_filepath);
       my ($volume, $dirs) = File::Spec->splitpath($dst_filepath);
       make_path($dirs);
       rename $src_filepath, $dst_filepath;
-      accept_file($item_ref->[0], $dst_filepath);
+      accept_file($item_ref->{hash}, $dst_filepath);
    }
 }
 
@@ -220,6 +243,18 @@ sub rename_dest_files {
    $filename =~ s/ /_/g;
    $filename = lc $filename;
    return $filename;
+}
+
+sub copy_special_file {
+   my ($filename, $dst_folder) = @_;
+   make_path($dst_folder);
+   if ($filename =~ /\.([^\.]+)$/) {
+      my $ext          = lc $1;
+      my $index        = get_special_index($dst_folder);
+      my $dst_filepath = File::Spec->catfile($dst_folder, sprintf "%04d.%s", $index, $ext);
+      say "special source '$filename'\tto '$dst_filepath'";
+      copy($filename, $dst_filepath) or die "Copy failed: $!";
+   }
 }
 
 sub check_dest {
@@ -234,6 +269,18 @@ sub check_dest {
       say "\t\t destination exists => $dst_filepath";
    }
    return $dst_filepath;
+}
+
+sub get_special_index {
+   my ($dst_folder) = @_;
+   my $max = -1;
+   opendir(my $dst_folderfh, $dst_folder) or die "unable to open '$dst_folder' $!";
+   while (readdir $dst_folderfh) {
+      if (/(\d+)\.\w+/) {
+         $max = $max > $1 ? $max : $1;
+      }
+   }
+   return $max + 1;
 }
 
 sub swipe {
@@ -258,24 +305,33 @@ sub swipe {
       my $j = $$i_ref + $buffer_size_down < $#to_sort ? $$i_ref + $buffer_size_down : $#to_sort;
       for my $k ($i .. $j) {
          if ($force) {
-            delete $to_sort[$k][3] if exists $to_sort[$k][3];
-            delete $to_sort[$k][4] if exists $to_sort[$k][4];
+            delete $to_sort[$k]{resizing} if exists $to_sort[$k]{resizing};
+            delete $to_sort[$k]{data}     if exists $to_sort[$k]{data};
+            delete $to_sort[$k]{ok}       if exists $to_sort[$k]{ok};
          }
-         unless (exists $to_sort[$k][3] and $to_sort[$k][3]) {
-            $queue_in->enqueue([$k, File::Spec->catfile($tmp_folderpath, $to_sort[$k][1]), $max{x}, $max{y}]);
-            $to_sort[$k][3] = 1;
+         unless (exists $to_sort[$k]{resizing} and $to_sort[$k]{resizing}) {
+            $queue_in->enqueue([$k, File::Spec->catfile($tmp_folderpath, $to_sort[$k]{filename}), $max{x}, $max{y}]);
+            $to_sort[$k]{resizing} = 1;
          }
       }
-      if ($i - 1 >= 0 and exists $to_sort[$i - 1][4]) {
-         delete $to_sort[$i - 1][3];
-         delete $to_sort[$i - 1][4];
+      if ($i - 1 >= 0 and exists $to_sort[$i - 1]{data}) {
+         delete $to_sort[$i - 1]{resizing};
+         delete $to_sort[$i - 1]{data};
+         delete $to_sort[$i - 1]{ok};
       }
       while (defined(my $item_ref = $queue_out->dequeue_nb())) {
-         $to_sort[$item_ref->[0]][4] = $item_ref->[1];
+         my $index = $item_ref->[0];
+         $to_sort[$index]{data}     = $item_ref->[2];
+         $to_sort[$index]{ok}       = $item_ref->[1];
+         $to_sort[$index]{resizing} = 0;
+
       }
-      while (not exists $to_sort[$$i_ref][4]) {
+      while (not exists $to_sort[$$i_ref]{data}) {
          my $item_ref = $queue_out->dequeue();
-         $to_sort[$item_ref->[0]][4] = $item_ref->[1];
+         my $index    = $item_ref->[0];
+         $to_sort[$index]{data}     = $item_ref->[2];
+         $to_sort[$index]{ok}       = $item_ref->[1];
+         $to_sort[$index]{resizing} = 0;
       }
    };
 
@@ -283,22 +339,31 @@ sub swipe {
    my $update_sub = sub {
       my ($force) = @_;
       &$loadpic_sub($force);
-      $mw->title("$filename ($$i_ref/$#to_sort) : $to_sort[$$i_ref][1]");
+
+      # make the title of the Main Windows
+      my $title = "$filename ($$i_ref/$#to_sort) : $to_sort[$$i_ref]{filename}";
+      if (exists $to_sort[$$i_ref]{special} and $to_sort[$$i_ref]{special}) {
+         $title .= " [" . $special_folders{$to_sort[$$i_ref]{special}}[0] . "]";
+      }
+      $mw->title($title);
       $canvas->delete(@items);
       $items[0] = $canvas->createRectangle(0,           0, $max{x} / 2, $max{y}, -fill => "red");
       $items[1] = $canvas->createRectangle($max{x} / 2, 0, $max{x},     $max{y}, -fill => "green");
-      my $image = $canvas->Photo(-data => $to_sort[$$i_ref][4], -format => 'png');
-      $items[2] = $canvas->createImage($max{x} / 2 + $max{x} / 2 * $to_sort[$$i_ref][2], $max{y} / 2, -image => $image);
+      if ($to_sort[$$i_ref]{ok}) {
+         my $image = $canvas->Photo(-data => $to_sort[$$i_ref]{data}, -format => 'png');
+         $items[2] = $canvas->createImage($max{x} / 2 + $max{x} / 2 * $to_sort[$$i_ref]{status}, $max{y} / 2, -image => $image);
+      }
+
    };
 
    my $goright_sub = sub {
       $canvas->move($items[2], $max{x} / 2, 0);
-      $to_sort[$$i_ref][2]++ if $to_sort[$$i_ref][2] < 1;
+      $to_sort[$$i_ref]{status}++ if $to_sort[$$i_ref]{status} < 1;
    };
 
    my $goleft_sub = sub {
       $canvas->move($items[2], -$max{x} / 2, 0);
-      $to_sort[$$i_ref][2]-- if $to_sort[$$i_ref][2] > -1;
+      $to_sort[$$i_ref]{status}-- if $to_sort[$$i_ref]{status} > -1;
    };
 
    my $goup_sub = sub {
@@ -315,7 +380,7 @@ sub swipe {
    };
 
    my $pass_sub = sub {
-      if ($to_sort[$$i_ref][2] != 0) {
+      if ($to_sort[$$i_ref]{status} != 0) {
          &$godown_sub();
       }
    };
@@ -323,7 +388,7 @@ sub swipe {
 
    my $execution_sub = sub {
       for my $i ($$i_ref .. $#to_sort) {
-         $to_sort[$i][2] = -1;
+         $to_sort[$i]{status} = -1;
       }
       $mw->destroy();
    };
@@ -336,6 +401,13 @@ sub swipe {
       &$update_sub(1);
    };
 
+   my $special_sub_factory = sub {
+      my ($key) = @_;
+      return sub {
+         $to_sort[$$i_ref]{special} = $key;
+         &$update_sub();
+      };
+   };
 
    $mw->bind('<KeyPress-d>',   $goright_sub);
    $mw->bind('<KeyRelease-d>', $godown_sub);
@@ -347,6 +419,10 @@ sub swipe {
    $mw->bind('<KeyRelease-e>', $execution_sub);
    $mw->bind('<KeyRelease-q>', sub { $mw->destroy() });
 
+   for my $key (keys %special_folders) {
+      $mw->bind("<KeyRelease-$key>", &$special_sub_factory($key));
+   }
+
    &$update_sub();
 
    MainLoop;
@@ -355,14 +431,14 @@ sub swipe {
    my @results;
    my $file_nb = 0;
    for my $item_ref (@to_sort) {
-      if ($item_ref->[2] == -1) {
-         reject_file($item_ref->[0], $item_ref->[1]);
+      if ($item_ref->{status} == -1) {
+         reject_file($item_ref->{hash}, $item_ref->{filename});
       }
-      elsif ($item_ref->[2] == 0) {
+      elsif ($item_ref->{status} == 0) {
          $file_nb++;
       }
-      elsif ($item_ref->[2] == 1) {
-         push @results, [$item_ref->[0], $item_ref->[1]];
+      elsif ($item_ref->{status} == 1) {
+         push @results, $item_ref;
       }
    }
    set_archive_remains($filename, $file_nb);
@@ -375,7 +451,7 @@ sub swipe {
 db_init();
 
 #many worker
-for (1..$threads_nb){
+for (1 .. $threads_nb) {
    threads->create('producepic');
 }
 if ($rebuild) {
